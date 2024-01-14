LineageOS/android_hardware_qcom_display

gralloc: Query GPU capability before allocating protected buffer
If GPU is not able to support protected content i.e. secure context
for hardware buffer, do not allow allocation to go through.

Change-Id: I3bc5406781cfba27648dfcff707b466d0b3b0c76

gralloc/gr_utils.h
@@ -91,6 +91,8 @@ unsigned int GetUBwcSize(int width, int height, int format, unsigned int aligned
                          unsigned int alignedh);
 int GetBufferLayout(private_handle_t *hnd, uint32_t stride[4],
                     uint32_t offset[4], uint32_t *num_planes);
+bool IsGPUSupportedHwBuffer(gralloc1_producer_usage_t prod_usage);
+
 }  // namespace gralloc1
 
 #endif  // __GR_UTILS_H__

tanz3/hardware_qcom_display

gralloc/gr_utils.h
@@ -230,6 +230,7 @@ bool HasAlphaComponent(int32_t format);
 void GetDRMFormat(uint32_t format, uint32_t flags, uint32_t *drm_format,
                   uint64_t *drm_format_modifier);
 bool CanAllocateZSLForSecureCamera();
+bool IsGPUSupportedHwBuffer(uint64_t usage);
 }  // namespace gralloc
 
 #endif  // __GR_UTILS_H__